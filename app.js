bb0d25872ae94196b8d4a8deab530860af4f0674
John Kelly <kellyj58@tcd.ie>
2016-07-08 16:25:10 +0100

    Added a basic test endpoint for search

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
+var crypto = require("crypto");
 var express = require("express");
 var app = express();
 app.use(express.static("static"));
 app.listen(80);
+app.get("/search", function(req, res) {
+  var numResults = 100;
+  res.send(generateDummyResults(numResults));
+});
+
+function generateDummyResults(num) {
+  var payload = [];
+  for(var i = 0; i < num; i++) {
+    var str = crypto.randomBytes(16).toString("base64");
+    payload.push(str);
+  }
+  return payload
+}
