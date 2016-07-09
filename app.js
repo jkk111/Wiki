ebcc016322b3edce9dc55c14632c9cd0c65d5dd5
John Kelly <kellyj58@tcd.ie>
2016-07-09 16:46:05 +0100

    Removed dummy data, added endpoints to search, no search functionality yet though

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var crypto = require("crypto");
 var express = require("express");
+var db = require("./util/db.js");
 var app = express();
 app.use(express.static("static"));
 app.listen(80);
@@ -16,3 +17,18 @@ function generateDummyResults(num) {
   }
   return payload
 }
+
+
+app.get("/wiki/:article", function(req, res) {
+  var article = db.fetch(req.params.article);
+  if(!article) {
+    res.send(notFound(req.params.article));
+  } else {
+    res.send(article);
+  }
+});
+
+app.get("/search", function(req, res) {
+  var results = db.fetch(req.query.q);
+  res.send(results);
+})
