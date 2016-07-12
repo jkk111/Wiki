1950c175f3e7fd671b4a5f08c8dfeee3f0fb693f
John Kelly <kellyj58@tcd.ie>
2016-07-12 02:09:44 +0100

    More accurate data sent from server

--- app.js
+++ app.js
@@ -5,19 +5,32 @@ var app = express();
 app.use(express.static("static"));
 app.listen(80);
 app.get("/search", function(req, res) {
-  var numResults = 100;
-  res.send(generateDummyResults(numResults));
+  var numResults = randomRange(50, 200);
+  res.send(generateDummyResults(numResults, req.query.q));
 });
 
-function generateDummyResults(num) {
+function generateDummyResults(num, query) {
   var payload = [];
   for(var i = 0; i < num; i++) {
-    var str = crypto.randomBytes(16).toString("base64");
-    payload.push(str);
+    var post = {};
+    post.title = computeRandomString(randomRange(16, 40));
+    post.body = computeRandomString(20, 400) + query + computeRandomString(20, 400);
+    payload.push(post);
   }
   return payload
 }
 
+function randomRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function computeRandomString(min, max) {
+  if(min == max || !max) {
+    return crypto.randomBytes(min).toString("base64");
+  }
+  var len = randomRange(min, max);
+  return crypto.randomBytes(len).toString("base64");
+}
 
 app.get("/wiki/:article", function(req, res) {
   var article = db.fetch(req.params.article);
